ffc5426cba0bb21fd94b27aca1e13fea96fae753
DennisCalderon <dennis.tacna@gmail.com>
2020-09-30 14:43:12 +0000

    Laratrust  - Lo instalamos con : composer require "santigarcor/laratrust:4.0.*"  - Documentación : https://laratrust.readthedocs.io/en/4.0/installation.html  - Luego debemos de configurarlo en el archivo: " config/app.php " , agregar su Providers(proveedor externo -> librería) y un Alias para instanciarlo(usarlo)  - Seguidamente usamos el comando : php artisan vendor:publish --tag="laratrust" para publicar nuestro vendor(publicar todos los archivos de configuración)  - Al ejecutar el comando de cinfiguración se crean 2 nuevos archivos con los cuales empezaremos a trabajar con la librería.

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laratrust\LaratrustServiceProvider::class,
+
     ],
 
     /*
@@ -223,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Laratrust' => Laratrust\LaratrustFacade::class, // Laratrust::xxxx -> uso en una instancia
 
     ],
 
